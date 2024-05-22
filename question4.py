a403f2c05844275b0dfd4b38c455765a48ede8b0
dineshr57 <ravidevidin234@gmail.com>
2024-05-22 11:30:20 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,7 @@
 #write a program to find the sum of digits of a given number'
+n=int(input("enter the number to be added"))
+sum=0
+for i in range(1,n+1):
+  num=int(input("enter the number"))
+  sum=sum+num
+  print("the given sum is:",sum)
